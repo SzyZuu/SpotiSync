c6f7d9a1f047bb62e466dad974b4ab47f882a84b
SzyZu <skwarek116@gmail.com>
2024-10-09 20:35:35 +0200

    feat: Basic log in Page with Bindings

---
+++ SpotiSync/ViewModels/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+
+namespace SpotiSync.ViewModels;
+
+public class LoginViewModel : ViewModelBase
+{
+    private bool _canContinue;
+
+    private string? _serverAddress;
+
+    public bool CanContinue
+    {
+        get { return _canContinue; }
+        protected set { this.RaiseAndSetIfChanged(ref _canContinue, value); }
+    }
+
+    public string? ServerAddress
+    {
+        get { return _serverAddress; }
+        set { this.RaiseAndSetIfChanged(ref _serverAddress, value); }
+    }
+    
+    private void UpdateCanContinue()
+    {
+        //check if Spotify is logged in and Server connection available
+    }
+}
--- SpotiSync/ViewModels/MainWindowViewModel.cs
+++ SpotiSync/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace SpotiSync.ViewModels;
+﻿using ReactiveUI;
+
+namespace SpotiSync.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    public MainWindowViewModel()
+    {
+        _currentPage = _pages[0];
+    }
     public string Greeting => "Mmmm Moosic";
+
+    private readonly ViewModelBase[] _pages =
+    {
+        new LoginViewModel()
+    };
+
+    private ViewModelBase _currentPage;
+
+    public ViewModelBase CurrentPage
+    {
+        get { return _currentPage; }
+        private set { this.RaiseAndSetIfChanged(ref _currentPage, value); }
+    }
 }
