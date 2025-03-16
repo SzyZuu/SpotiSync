c79202a583cc6137ee13ba5ab18898054944735a
SzyZu <skwarek116@gmail.com>
2025-03-16 18:40:58 +0100

    fix: spotify auth working now

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import Image from "next/image";
 import { useSession, signIn} from "next-auth/react";
 import LogInButton from "@/components/LogInButton";
 
 export default function Home() {
+    /*const session = useSession();
+    console.log(session);*/
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-work-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,25 @@
-import { NextAuthOptions } from "next-auth";
+import {type NextAuthOptions, Session} from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import {JWT} from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
     providers: [
         SpotifyProvider({
-            clientId: process.env.SPOTIFY_ID,
-            clientSecret: process.env.SPOTIFY_SECRET
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET
         }),
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, account }: { token: JWT; account: any }): Promise<JWT> {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+
+        async session({ session, token }: { session: Session; token: JWT }): Promise<Session> {
+            (session as any).accessToken = token.accessToken;
+            return session;
+        },
+    }
 }
